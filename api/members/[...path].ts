4964818076b5af3eaffb615829d31aa0e0ff1db6
sultan101004 <sultanshah101004@gmail.com>
2026-06-19 03:00:07 +0500

    fix: merge donations summary and donor routes into catch-all to stay under Vercel 12 function limit

--- api/members/[...path].ts
+++
@@ -1,9 +0,0 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
-import handler from '../misc/[...path]';
-
-export default async (req: VercelRequest, res: VercelResponse) => {
-  const originalUrl = req.url!;
-  req.url = originalUrl.replace('/api/members', '/api/misc');
-  req.query.path = ['members', ...((req.query.path as string[]) || [])];
-  return handler(req, res);
-};
--- api/tiers/[...path].ts
+++
@@ -1,9 +0,0 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
-import handler from '../misc/[...path]';
-
-export default async (req: VercelRequest, res: VercelResponse) => {
-  const originalUrl = req.url!;
-  req.url = originalUrl.replace('/api/tiers', '/api/misc');
-  req.query.path = ['tiers', ...((req.query.path as string[]) || [])];
-  return handler(req, res);
-};
