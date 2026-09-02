8675f91ab49c4d43818d4287231aa00a0a5464d1
Omnimap Auditor <auditor@mykstudio.com>
2026-09-02 11:56:54 +0500

    chore: remove test script

--- test-gallery.mjs
+++
@@ -1,8 +0,0 @@
-
-import fetch from 'node-fetch';
-
-async function test() {
-  console.log('Use node-fetch if we had an admin token, but we don\'t. We can test with supabase directly to make sure it inserts.');
-}
-test();
-
